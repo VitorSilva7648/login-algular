89aa1940419ebc4c12ee988d3d226b2aeff423c2
Vitor Hugo Silva Gonçalves <vitorhugo662@hotmail.com>
2025-06-09 13:57:35 -0300

    create-account

--- login/src/app/app.routes.ts
+++ login/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
-import { Cad } from './pages/cad/cad';
+import { Register } from './pages/register/register';
 
 export const routes: Routes = [
     {path: "", component: Home},
-    {path: "cad", component: Cad}
+    {path: "register", component: Register}
 ];
--- login/src/app/pages/register/register.spec.ts
+++ login/src/app/pages/register/register.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Cad } from './cad';
+import { Register } from './register';
 
-describe('Cad', () => {
-  let component: Cad;
-  let fixture: ComponentFixture<Cad>;
+describe('Register', () => {
+  let component: Register;
+  let fixture: ComponentFixture<Register>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Cad]
+      imports: [Register]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Cad);
+    fixture = TestBed.createComponent(Register);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
