a5b115bccbe3f303d42e0cc6b37c697ff6e1b65e
Vitor Hugo Silva Gonçalves <vitorhugo662@hotmail.com>
2025-06-09 15:00:33 -0300

    update

--- login/src/app/pages/register/register.ts
+++ login/src/app/pages/register/register.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.scss'
 })
 export class Register {
-
+  
+  selectedPlano: string = ''; // ou number, se preferir
+  
+  
 }
