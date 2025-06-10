bd72546f0456f41057cade274ecd7be52a6b6a5d
Vitor Hugo Silva Gonçalves <vitorhugo662@hotmail.com>
2025-06-09 22:00:10 -0300

    mockup payment

--- login/src/app/pages/payment/payment.ts
+++ login/src/app/pages/payment/payment.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-payment',
-  imports: [RouterModule, FormsModule],
+  imports: [RouterModule, FormsModule, CommonModule],
   templateUrl: './payment.html',
   styleUrl: './payment.scss'
 })
 export class Payment {
   selectedPlano: string = '';
+  selectedPayment: string = '';
 }
